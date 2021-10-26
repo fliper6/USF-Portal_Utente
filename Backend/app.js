9e8f018a0131e1ef0c82844967cad7e436bad5b4
Válter Carvalho <47797461+wurzy@users.noreply.github.com>
2021-10-26 16:29:27 +0100

    backend

---
+++ Backend/app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/USF_PORTAL_UTENTE', 
+      { useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 5000});
+  
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erro de conexão ao MongoDB...'));
+db.once('open', function() {
+  console.log("Conexão ao MongoDB realizada com sucesso.")
+});
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
