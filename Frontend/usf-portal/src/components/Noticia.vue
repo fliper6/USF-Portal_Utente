dda584e1d404a95681d0a8b9629eb9588a27c818
CoelhoLado <naac.1999@gmail.com>
2021-11-11 12:03:28 +0000

    added noticias
    rever estrutura?

---
+++ Frontend/usf-portal/src/components/Noticia.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <div class="meta"> a {{noticia.data}} por {{noticia.autor}} </div>
+    <div class="title">{{noticia.title}}</div>
+    <div class="text" v-for="(line,n) in noticia.text.split('\n')" :key="n">{{line}}</div>
+    <div class="attachments">
+      <img class="image" v-for="(attachment,n) in noticia.attachments" :key="n" :src='attachment'/>
+    </div>
+  </div>
+</template>  
+
+<script>
+export default {
+  name: 'Noticia',
+  props: ["noticia"],
+  mounted () {
+    console.log(this.noticia.text.split('\n'))
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  width: 80vw;
+  border-bottom: 1px solid #800000;
+  padding-bottom: 30px;
+}
+
+.meta {
+  font-size: 0.7em !important;
+  color: gray;
+  margin-left:3px;
+}
+
+.title {
+  font-size: 2em !important;
+  color: #800000;
+  margin-bottom: 15px;
+}
+
+.text {
+  margin-bottom: 20px;
+}
+
+.attachments {
+  display: flex;
+  justify-content: center;
+}
+
+.image {
+  max-height: 400px;
+  max-width: 800px;
+}
+</style>
--- Frontend/usf-portal/src/views/Home.vue
+++ Frontend/usf-portal/src/views/Home.vue
@@ -1,14 +1,51 @@
 <template>
   <div class="home">
-    <h1>Página das noticias</h1>
+    <Noticia v-for="(noticia,n) in noticias" :noticia='noticia' :key="n" />
   </div>
 </template>
 
 <script>
+import Noticia from "../components/Noticia.vue"
 
 export default {
   name: 'Home',
   components: {
+    Noticia
+  },
+  data () {
+    return {
+      noticias: [
+        {
+          title: "Conflitos éticos",
+          text: `Sabia que existe a possibilidade de pedir parecer sobre questões éticas na saúde e no âmbito da investigação clinica, à Comissão de Ética para a Saúde (CES)?
+
+Estes conflitos éticos podem surgir em questões de respeito pela dignidade humana, integridade pessoal, justiça e a equidade, liberdade e a autonomia, entre outro.
+
+O nosso panfleto pretende ajudar a esclarecê-lo! Não hesite em consultá-lo. `,
+          attachments: ["https://1.bp.blogspot.com/-BREapjKunuI/YC5aGcDTuUI/AAAAAAAAHQg/xPNCACPL-bEcUseejFryoUTXJ0u-zCQSwCNcBGAsYHQ/s518/7346147_CONFLITO.jpg"],
+          autor: "Jorge Costa",
+          data: "11/11/2021"
+        },
+        {
+          title: "DIA MUNDIAL DA SAÚDE",
+          text: `Sabia que hoje comemoramos o dia mundial da Saúde?
+
+Nesse sentido, a Direção Geral de Saúde preparou uma sessão dedicada a este dia. 
+
+A sessão pode ser visualizada no seguinte link.`,
+          attachments: ["https://1.bp.blogspot.com/-uXs9ssoNCx8/YG4ey3Dyo9I/AAAAAAAAHUw/EDNXGwVmY5cd71cA49tfDp2f6F1tZDPpQCNcBGAsYHQ/s1920/169976392_3971002472922344_6567909077237891199_n.jpg"],
+          autor: "Jorge Costa",
+          data: "13/07/2021"
+        },
+        {
+          title: "GUIA PARA LANCHES SAUDÁVEIS!",
+          text: `A Direção-Geral da Saúde e a Direção-Geral da Educação publicam hoje um guia sobre lanches escolares saudáveis, assinalando o regresso ao ensino presencial de muitas crianças e jovens. Pode consultar o manual no seguinte link.`,
+          attachments: ["https://1.bp.blogspot.com/-bIAE5avXsDg/YGudKRVlRCI/AAAAAAAAHUY/sR-ID2R4qEkeW1eDJU6kruOkoxEtwTghgCNcBGAsYHQ/s789/Guia_Lanches.png"],
+          autor: "Ana João",
+          data: "31/11/2020"
+        }
+      ]
+    }
   }
 }
 </script>
