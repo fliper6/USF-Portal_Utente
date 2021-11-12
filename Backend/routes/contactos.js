9bb33dfc4d496a1fdec043eef4c927b22c638811
Válter Carvalho <47797461+wurzy@users.noreply.github.com>
2021-11-12 18:58:37 +0000

    pasta

--- Backend/routes/contactos.js
+++ Backend/routes/contactos.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 let Contacto = require('../controllers/contacto')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Contacto.listar()
