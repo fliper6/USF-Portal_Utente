b70df870da43c8e2563b01adeaf984f144f3195c
CoelhoLado <naac.1999@gmail.com>
2021-11-13 17:39:35 +0000

    Added html result to editor

--- Frontend/src/components/Editor.vue
+++ Frontend/src/components/Editor.vue
@@ -1,19 +1,49 @@
 <template>
-  <editor-content :editor="editor" />
+  <div>
+    <bubble-menu
+      class="bubble-menu"
+      :tippy-options="{ duration: 100 }"
+      :editor="editor"
+      v-if="editor"
+    >
+      <button @click="editor.chain().focus().toggleBold().run()" :class="{ 'is-active': editor.isActive('bold') }">
+        Bold
+      </button>
+      <button @click="editor.chain().focus().toggleItalic().run()" :class="{ 'is-active': editor.isActive('italic') }">
+        Italic
+      </button>
+      <button @click="editor.chain().focus().toggleStrike().run()" :class="{ 'is-active': editor.isActive('strike') }">
+        Strike
+      </button>
+    </bubble-menu>
+    <editor-content class="editor" :editor="editor" />
+    <button class="see" v-on:click="printHTML">See HTML</button>
+  </div>
 </template>
 
 <script>
-import { Editor, EditorContent } from '@tiptap/vue-2'
+import {
+  Editor,
+  EditorContent,
+  BubbleMenu
+} from '@tiptap/vue-2'
 import StarterKit from '@tiptap/starter-kit'
 
 export default {
   components: {
     EditorContent,
+    BubbleMenu
   },
 
   data() {
     return {
       editor: null,
+    }
+  },
+
+  methods: {
+    printHTML () {
+      console.log(this.editor.getHTML())
     }
   },
 
@@ -31,3 +61,17 @@ export default {
   },
 }
 </script>
+<style>
+.see {
+  background: var(--primary-color);
+  border-radius: 5px;
+  padding: 5px;
+  color:white
+}
+.editor {
+  border: var(--primary-color) solid 1px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  padding-bottom: 0px;
+}
+</style>
