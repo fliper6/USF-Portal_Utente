60574274a8a3d88fe61962104285a79bc6159767
Hugo Cardoso <ha.cardoso30@gmail.com>
2021-11-21 16:43:22 +0000

    fix

--- Backend/models/documento.js
+++ Backend/models/documento.js
@@ -4,7 +4,7 @@ var documentoSchema = new mongoose.Schema({
     titulo: {type: String, required: true},
     data_publicacao: {type: String, required: true},
     visibilidade: {type: Boolean, required: true},
-    _id_autor: {type: String, required: true},
+    _id_autor: {type: mongoose.Schema.Types.ObjectId, ref:"user", required: true},
     nome_autor: {type: String, required: true},
     id_categoria: {type: String, required: true},
     ficheiro: {type: {
