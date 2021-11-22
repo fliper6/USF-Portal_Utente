d5baad16a5afe45b1b395953e63a4f2f0cb67237
ricsmc <39051605+ricsmc@users.noreply.github.com>
2021-11-22 15:15:21 +0000

    perfil editavel

--- Frontend/src/views/Perfil.vue
+++ Frontend/src/views/Perfil.vue
@@ -1,7 +1,7 @@
   <!-- Vue SFC -->
 <template>
   <div class="perfil">
-    <v-container>
+    <v-container v-if="!this.editar">
       <v-row>
         <v-col offset =2 cols=2>
           <v-avatar size=120>
@@ -20,6 +20,54 @@
           <v-row>
             <h3>Email:<span class="infos">{{this.email}}</span></h3>
           </v-row>
+        </v-col>
+        <v-col>
+          <v-btn icon @click="edita"><v-icon dark>
+          mdi-pencil
+        </v-icon></v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-divider/>
+      </v-row>
+      
+    </v-container>
+    <v-container v-else>
+      <v-row>
+        <v-col offset =2 cols=2>
+          <v-avatar size=120>
+            <v-icon dark size=120 color="var(--grey3-color)">
+              mdi-account-circle-outline
+            </v-icon>
+          </v-avatar>
+        </v-col>
+        <v-col>
+          <v-row>
+            <v-text-field
+            v-model="nome"
+            label="Nome"
+            required
+          ></v-text-field>
+          </v-row>
+          <v-row>
+            <v-text-field
+            v-model="num"
+            label="Número SNS"
+            required
+            ></v-text-field>
+          </v-row>
+          <v-row>
+            <v-text-field
+            v-model="email"
+            label="E-Mail"
+            required
+          ></v-text-field>
+          </v-row>
+        </v-col>
+        <v-col>
+          <v-btn icon @click="save"><v-icon dark>
+          mdi-content-save
+        </v-icon></v-btn>
         </v-col>
       </v-row>
       <v-row>
@@ -77,9 +125,10 @@
           </v-col>
         </v-row>
         <v-row>
-          <v-divider>
+          <v-col><v-divider>
 
-          </v-divider>
+          </v-divider></v-col>
+          
         </v-row>
         <v-row>
           <v-col><h3>09/11/21</h3></v-col>
@@ -119,23 +168,33 @@ import axios from 'axios'
         email:'',
         nivel:'',
         medicacao:'',
+        editar:false,
         
       }
     },
     created(){
     if (this.token) {
       this.id = jwt.decode(this.token)._id
-      this.nome = jwt.decode(this.token).nome
-      console.log('oi')
-      this.email = jwt.decode(this.token).email
-      this.num = jwt.decode(this.token).nr_utente
-      this.nivel = jwt.decode(this.token).nivel
-      axios.get("http://localhost:3333/medicacao/historico/" + this.id, {headers:{'authorization':'Bearer '+ this.token}})
+      axios.get("http://localhost:3333/users/info/" + this.id,{headers:{'authorization':'Bearer '+ this.token}})
+        .then(data => {
+          console.log(data.data)
+          this.nome = data.data.nome
+          this.email = data.data.email
+          this.num = data.data.nr_utente
+          this.nivel = data.data.nivel
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      
+      
+      axios.get("http://localhost:3333/medicacao/historico/" + this.num, {headers:{'authorization':'Bearer '+ this.token}})
         .then( data => {
           this.medicacao = data.data
           console.log(data.data)
         })
-        .catch(() => {
+        .catch(err => {
+          console.log(err)
         })
     }
     },
@@ -154,6 +213,25 @@ import axios from 'axios'
       this.med = true
       this.cons = true
       this.sug = false
+    },
+    edita(){
+      this.editar = true
+    },
+    save(){
+      var data = {}
+      data['nome'] = this.nome
+      data['nr_utente'] = this.num
+      data['email'] = this.email
+      axios.put("http://localhost:3333/users/alterar/" + this.id, data,{headers:{'authorization':'Bearer '+ this.token}})
+        .then( data => {
+          this.medicacao = data.data
+          console.log(data.data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      this.editar = false
+      this.$router.go()
     }
   }
   
