d823f8a67a0f7eccd08a2c51f6edefd593d39329
fliper6 <filipa.alvesantos@outlook.com>
2021-11-24 07:08:39 +0000

    ap analise mercado + verificação ao adicionar doc started

--- Frontend/src/views/Files.vue
+++ Frontend/src/views/Files.vue
@@ -4,69 +4,79 @@
       <v-col>
         <v-dialog v-model="dialog" width="750" style="overflow-x: hidden;">
           <template v-slot:activator="{ on, attrs }">
-            <v-btn  class="button-principal" v-bind="attrs" v-on="on">
+            <v-btn class="button-principal" @click = "close()" v-bind="attrs" v-on="on">
               + Novo Documento
             </v-btn>
           </template>
+            <form>
+              <v-card>
+                <v-card-title class="text-h5 grey lighten-2"> Carregar novo documento </v-card-title> <br/>
+                <v-col style="margin: auto; padding: 0px 50px;">
 
-          <v-card>
-            <v-card-title class="text-h5 grey lighten-2"> Carregar novo documento </v-card-title> <br/>
+                  <v-text-field @input="$v.titulo.$touch()" @blur="$v.titulo.$touch()" :error-messages="tituloErrors" v-model="titulo" label="Título"></v-text-field> <br/>
+                  
+                  <p style="margin-bottom: 5px; color:#666666">Categoria associada:</p>
+                  <v-row style="height: 55px;">
+                    <v-col cols="11">
+                      <treeselect
+                        @input="$v.arvore.$touch()" 
+                        @open="warning_arvore = false"
+                        :error-messages="arvoreErrors"
+                        :max-height="100"
+                        :multiple="false" :options="options" 
+                        :flatten-search-results="true"
+                        :normalizer="normalizer"
+                        v-model="arvore"
+                        placeholder="Tags"/> 
+                      <span style="color: #ff5252; font-size: 12px;" v-if="this.warning_arvore">Categoria é um campo obrigatório.</span>
+                    </v-col>  
+                    <br/>
+                    <v-col cols="1">
+                      <v-dialog v-model="dialog2" width="400">
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-btn style="min-width:25px !important; height:34px !important; background-color:#e0e0e0;" v-bind="attrs" v-on="on">
+                            +
+                          </v-btn>
+                        </template>
 
-            <v-col style="margin: auto; padding: 0px 50px;">
-              <v-text-field v-model="titulo" :counter="50" label="Título" required></v-text-field>
+                        <v-card>
+                          <v-card-title class="text-h5 grey lighten-2"> Adicionar nova categoria</v-card-title> <br/>
+                          <v-col style="margin: auto; padding: 0px 50px;">
+                            <p style="margin-bottom: 5px; color:#666666">Ramo da categoria</p>
+                            <treeselect
+                              v-model="value2"
+                              :max-height="100"
+                              :multiple="false" :options="options" 
+                              :flatten-search-results="true"
+                              :normalizer="normalizer"
+                              placeholder="Tags"/> <br/>
 
-              <p style="margin-bottom: 5px; color:#666666">Categoria associada:</p>
-              <v-row style="height: 55px;">
-                <v-col cols="11">
-                  <treeselect
-                    v-model="arvore"
-                    :max-height="100"
-                    :multiple="false" :options="options" 
-                    :flatten-search-results="true"
-                    placeholder="Tags"/> <br/>
-                </v-col>  
-                <v-col cols="1">
-                  <v-dialog v-model="dialog2" width="400">
-                    <template v-slot:activator="{ on, attrs }">
-                      <v-btn style="min-width:25px !important; height:34px !important; background-color:#e0e0e0;" v-bind="attrs" v-on="on">
-                        +
-                      </v-btn>
-                    </template>
+                            <v-text-field v-model="titulo" :counter="50" label="Nome da categoria"></v-text-field> <br/>
+                          </v-col>
+                          <v-divider></v-divider>
 
-                    <v-card>
-                      <v-card-title class="text-h5 grey lighten-2"> Adicionar nova categoria</v-card-title> <br/>
-                      <v-col style="margin: auto; padding: 0px 50px;">
-                        <p style="margin-bottom: 5px; color:#666666">Ramo da categoria</p>
-                        <treeselect
-                          v-model="value2"
-                          :max-height="100"
-                          :multiple="false" :options="options" 
-                          :flatten-search-results="true"
-                          placeholder="Tags"/> <br/>
+                          <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn class="button-cancelar" text @click="dialog2 = false"> Cancelar </v-btn>
+                            <v-btn class="button-confirmar" text @click="addCategoria()"> Confirmar </v-btn>
+                          </v-card-actions> 
+                        </v-card>
+                      </v-dialog> 
+                    </v-col> 
+                  </v-row> <br/>
 
-                        <v-text-field v-model="titulo" :counter="50" label="Nome da categoria" required></v-text-field> <br/>
-                      </v-col>
-                      <v-divider></v-divider>
+                  <v-file-input truncate-length="15" v-model="file"></v-file-input>
 
-                      <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn class="button-cancelar" text @click="dialog2 = false"> Cancelar </v-btn>
-                        <v-btn class="button-confirmar" text @click="addCategoria()"> Confirmar </v-btn>
-                      </v-card-actions> 
-                    </v-card>
-                  </v-dialog> 
-                </v-col> 
-              </v-row> <br/>
-              <v-file-input truncate-length="15" v-model="file"></v-file-input>
-            </v-col>
-            <v-divider></v-divider>
+                </v-col>
+                <v-divider></v-divider>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn class="button-cancelar" text @click="dialog = false"> Cancelar </v-btn>
-              <v-btn class="button-confirmar" text @click="addDocumento()"> Confirmar </v-btn>
-            </v-card-actions> 
-          </v-card>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn class="button-cancelar" text @click="close()"> Cancelar </v-btn>
+                  <v-btn class="button-confirmar" text @click="addDocumento()"> Confirmar </v-btn>
+                </v-card-actions> 
+              </v-card>
+            </form>
         </v-dialog> 
       </v-col>
       <v-col style="max-width:400px">
@@ -76,6 +86,7 @@
           :max-height="200"
           :multiple="true" :options="options" 
           :flatten-search-results="true"
+          :normalizer="normalizer"
           placeholder="Filtar por..."/> <br/>
       </v-col>
     </v-row>
@@ -93,10 +104,22 @@
   import '@riophae/vue-treeselect/dist/vue-treeselect.css'
   import axios from 'axios'
   import jwt from 'jsonwebtoken'
+  import { validationMixin } from 'vuelidate'
+  import { required } from 'vuelidate/lib/validators'
 
   export default {
+    mixins: [validationMixin],
     name: "Files",
-    components: { Treeselect },
+    components: {
+      Treeselect,
+    },
+
+    validations: {
+      titulo: { required },
+      arvore: { required },
+      file: { required },
+    },
+
     data() {
       return {
         token: localStorage.getItem('jwt'),
@@ -107,9 +130,9 @@
 
         /* + DOCUMENTO */
         dialog: false,
-        nome: null,
         titulo: null,
         arvore: null,
+        warning_arvore: false,
         file: null,
 
         /* + CATEGORIA */
@@ -133,50 +156,97 @@
         ]
       }
     },
+    
+    computed: {
+      tituloErrors () {
+        const errors = []
+        if (!this.$v.titulo.$dirty) return errors
+        !this.$v.titulo.required && errors.push('Título é um campo obrigatório.')
+        return errors
+      },
+      arvoreErrors () {
+        const errors = []
+        if (!this.$v.arvore.$dirty) return errors
+        !this.$v.arvore.required && errors.push('Categoria é um campo obrigatório.')
+        //this.warning_arvore = true
+        return errors
+      },
+    },
+
     methods: {
+       // TO BE FIXED
+        filtrar: function () {
+          if(this.valueFiltro.length > 0) { 
+            this.docsfiltrados = []
+            for(var i = 0; i < this.docs.length; i++) {
+              var intersecao = (this.docs[i].categoria).filter(value => (this.valueFiltro).includes(value));
+              console.log(intersecao)
+              if(intersecao.length > 0) 
+                this.docsfiltrados.push(this.docs[i])
+            }
+          }
+          else // se tiver vazio, não filtra
+            this.docsfiltrados = this.docs
+        },
+        normalizer(node) { 
+          return { children: node.children && node.children.length ? node.children : 0 } 
+        },
         addCategoria: function () {
           this.dialog2 = false;
         },
+        close() {
+          this.$v.$reset()
+          this.titulo = null
+          this.arvore = null
+          this.file = null
+          this.warning_arvore = false
+          this.dialog = false
+        },
         addDocumento: function () {
-          this.dialog = false;
+          this.$v.$touch()
+          if (!this.$v.arvore.required)
+            this.warning_arvore = true
+          if (this.$v.titulo.required && this.$v.arvore.required) {
+            this.dialog = false;
 
-          let formData = new FormData();
-          formData.append('documento', this.file)
-          formData.append('titulo', this.titulo)
-          formData.append('id_categoria', this.arvore)
+            console.log(jwt.decode(this.token).nivel)
+            
+            let formData = new FormData();
+            formData.append('documento', this.file)
+            formData.append('titulo', this.titulo)
+            formData.append('id_categoria', this.arvore)
 
-          console.log(jwt.decode(this.token).nivel)
+            console.log(jwt.decode(this.token).nivel)
 
-          axios.post("http://localhost:3333/documentos/", 
-             formData, 
-             {
-                headers: {
-                  'Content-Type': 'multipart/form-data', 
-                  'Authorization': 'Bearer ' + localStorage.getItem('jwt')
-                }
-             }
-            ).then(() => {
-              console.log("Ficheiro uploaded com sucesso!")
+            axios.post("http://localhost:3333/documentos/", 
+              formData, 
+              {
+                  headers: {
+                    'Content-Type': 'multipart/form-data', 
+                    'Authorization': 'Bearer ' + localStorage.getItem('jwt')
+                  }
+              }
+              ).then(() => {
+                console.log("Ficheiro uploaded com sucesso!")
 
-                // Atualizar documentos
-                axios.get("http://localhost:3333/documentos")
-                  .then(data => {
-                    this.docs = data.data
-                    this.docs.forEach(item => {
-                      item.data_publicacao = item.data_publicacao.slice(0,10)
-                      item.ficheiro.nome_ficheiro = item.ficheiro.nome_ficheiro.split(".")[1]
+                  // Atualizar documentos
+                  axios.get("http://localhost:3333/documentos")
+                    .then(data => {
+                      this.docs = data.data
+                      this.docs.forEach(item => {
+                        item.data_publicacao = item.data_publicacao.slice(0,10)
+                        item.ficheiro.nome_ficheiro = item.ficheiro.nome_ficheiro.split(".")[1]
+                      })
+                      this.docsfiltrados = this.docs
                     })
-                    this.docsfiltrados = this.docs
-                  })
-                  .catch(() => {
-                    console.log("Ocorreu um erro ao obter a listagem de documentos.")
-                  })
-                  
-            }).catch(() => {
-              console.log("Ocorreu um erro ao obter ao dar upload ao documento.")
-            }) 
-
-          
+                    .catch(() => {
+                      console.log("Ocorreu um erro ao obter a listagem de documentos.")
+                    })
+                    
+              }).catch(() => {
+                console.log("Ocorreu um erro ao obter ao dar upload ao documento.")
+              })  
+          }
         },
         mudarVisibilidade: function () {
         },
@@ -185,6 +255,7 @@
 
     },
     created() {
+      
       // Obter lista de documentos
       axios.get("http://localhost:3333/documentos")
         .then(data => {
