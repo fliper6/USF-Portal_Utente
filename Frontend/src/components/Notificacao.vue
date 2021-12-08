b8b7edc7e1d40ba18da2740ca17a9b1972213fc1
LuisRamos99 <48031198+LuisRamos99@users.noreply.github.com>
2021-12-08 00:09:39 +0000

    Melhorado o aspeto nas notificações

--- Frontend/src/components/Notificacao.vue
+++ Frontend/src/components/Notificacao.vue
@@ -1,20 +1,55 @@
 <template>
     <v-menu offset-y>
-        <template v-slot:activator="{ }">
-         <!-- <div @click="up()" v-bind="attrs" v-on="on">-->
-          <span v-on:click="contador++">
-            <notification-bell :size='25' fontSize="10px" iconColor='var(--grey3-color)' :ding='true' :count="contador" :upperLimit='10' />
+        <template v-slot:activator="{ on }">
+          <span v-on="on" style="cursor:pointer" @click="openNotifications()">
+            <notification-bell :size='25' fontSize="10px" iconColor='var(--grey3-color)' :ding='pling' :count="contador" :upperLimit='10' />
           </span>
         </template>
-        <v-list style="padding:0">
-          <router-link class="opcao" :to="'/'">
-            <v-list-item class="opcao">
-              <b>Notificação</b>
-            </v-list-item>
-          </router-link>
-        </v-list>
+
+        <v-card max-width=400px> 
+          <v-list max-height=800px dense class="pa-0">
+
+            <v-subheader  class="pa-8" style="font-size:20px">
+              <b style="color: var(--white)">Notificações</b>
+            </v-subheader>
+
+            <v-divider></v-divider>
+
+            <div v-if="this.notifications.length">
+              <v-list-item 
+                @click="goToNotification(notification.idReferente)" 
+                class="pa-6 backColor" 
+                v-for="notification in notifications" 
+                :key="`notification-key-${notification._id}`"
+              >
+                  <v-list-item-icon>
+                    <v-icon v-if="notification.estado==2">mdi-brightness-1</v-icon>
+                    <v-icon v-else color="var(--blue-color)">mdi-brightness-1</v-icon>
+                  </v-list-item-icon>
+                  <v-row>
+                    <v-list-item-content>
+                      <v-list-item-title v-text="notification.descricao" style="font-size:18px"></v-list-item-title>
+                      <div style="margin-top:10px; font-size:14px"> {{ notification.data_criacao | moment("from") }}</div>
+                    </v-list-item-content>           
+                  </v-row>
+              </v-list-item>
+            </div>
+            
+            <div v-else>
+              <v-list-item class="pa-6 backColor">
+                  <v-row>
+                    <v-list-item-content>
+                      <v-list-item-title style="font-size:18px">Não existem notificações!</v-list-item-title>
+                    </v-list-item-content>           
+                  </v-row>
+              </v-list-item>
+            </div>
+
+          </v-list>
+        </v-card>
     </v-menu>
 </template>
+
 
 <script>
 import NotificationBell from 'vue-notification-bell'
@@ -27,46 +62,66 @@ import axios from 'axios'
         name: "notificacao",
         data: () => ({
             token: localStorage.getItem('jwt'),
-            contador: 2,
-            socket: null
+            contador: 0,
+            socket: null,
+            notifications: [],
+            pling:false
         }),  
         components: {
           NotificationBell 
         },
         created() {
-          //SocketioService.setupSocketConnection();
+          //ao iniciar vai buscar as notificações iniciais
+          this.getNotificacoes()
+
+          //SocketioService.setupSocketConnection(); -> código antigo
 
           this.socket = io(process.env.VUE_APP_SOCKET_ENDPOINT,{
             query: {
               uid: jwt.decode(this.token)._id
             }
           });
-          //this.socket = io(process.env.VUE_APP_SOCKET_ENDPOINT);
+          //this.socket = io(process.env.VUE_APP_SOCKET_ENDPOINT); -> código antigo
           this.socket.on('connect', () => {
-            console.log(this.socket.id); // an alphanumeric id...
+            console.log(this.socket.id); 
           });
 
-          this.socket.on('update notificacoes', (data) => {
-            console.log(data);
+          this.socket.on('update notificacoes', () => {
+            this.pling=true
             this.getNotificacoes()
           });
         },
         beforeUnmount() {
-          //SocketioService.disconnect();~
+          //SocketioService.disconnect(); -> código antigo
           if (this.socket) {
             this.socket.disconnect();
           }
         },    
         methods: {
           getNotificacoes (){
-            let user = jwt.decode(this.token)._id
-            axios.get("http://localhost:3333/notificacao/PorUser/"+user,{headers:{'authorization':'Bearer '+ this.token}})
+            axios.get("http://localhost:3333/notificacao/PorUser/"+jwt.decode(this.token)._id, {headers:{'authorization':'Bearer '+ this.token}})
             .then( data => {
               console.log(data.data)
+              this.notifications = data.data
+              this.contador = this.getCount(this.notifications)
             })
             .catch(err => {
               console.log(err)
             })
+          },
+          getCount(lista) {
+            var c = 0
+            lista.forEach(elem => { if (elem.estado==0) c++ });
+            return c
+          },
+          openNotifications(){
+            //this.contador=0
+            //meter as notificações no estado 1
+          },
+          goToNotification(id){
+            console.log(id)
+            //meter a notificacao no estado 2
+            //redirecionar o utilizador para o sitio referente à notificação
           }
         }    
       }
@@ -74,5 +129,12 @@ import axios from 'axios'
 
 <style>
 
+.v-subheader {
+  background-color: var(--grey3-color) !important;
+}
+
+.backColor {
+  background-color: var(--grey2-color) !important;
+}
 
 </style>
