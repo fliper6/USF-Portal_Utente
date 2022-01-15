75a5b2a9dd3c257ee8131d7bd04df192545b8dc1
LuisRamos99 <48031198+LuisRamos99@users.noreply.github.com>
2022-01-15 13:45:20 +0000

    ...

--- Frontend/src/views/Error.vue
+++ Frontend/src/views/Error.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="error">
-    <h1>404 - Não foi possivel obter a página</h1>
+  <div >
+    <v-row align="center" justify="center">
+      <span style="font-size:140px; margin-top:100px; color:var(--primary-color)">
+        <b >404</b>
+      </span>
+    </v-row>
+    <v-row align="center" justify="center">
+       <b style="font-size:40px;">Oops! Esta página não existe 😝</b>
+    </v-row>
+   
   </div>
 </template>
--- Frontend/src/views/Forbidden.vue
+++ Frontend/src/views/Forbidden.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="error">
-    <h1>403 - Não tem premissão para aceder a esta página</h1>
+  <div >
+    <v-row align="center" justify="center">
+      <span style="font-size:140px; margin-top:100px; color:var(--primary-color)">
+        <b >403</b>
+      </span>
+    </v-row>
+    <v-row align="center" justify="center">
+       <b style="font-size:40px;">Oops! Não tem autorização para aceder a esta página 😅</b>
+    </v-row>
+   
   </div>
 </template>
+
