2989cabb252496d7932a71c5832abaea09fca4b5
ricsmc <39051605+ricsmc@users.noreply.github.com>
2022-01-16 19:06:09 +0000

    teste pls

--- Frontend/src/views/About.vue
+++ Frontend/src/views/About.vue
@@ -294,13 +294,7 @@ export default {
     }
   },
   mounted () {
-    // At this point, the child GmapMap has been mounted, but
-    // its map has not been initialized.
-    // Therefore we need to write mapRef.$mapPromise.then(() => ...)
-
-    this.$refs.mapRef.$mapPromise.then((map) => {
-      map.panTo({lat: 1.38, lng: 103.80})
-    })
+    
   },
   created(){
     
