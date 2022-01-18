ba7431418a5ed513be418152d10096f77acc6e71
CoelhoLado <naac.1999@gmail.com>
2022-01-18 19:59:40 +0000

    Fix submeter sugestões

--- Frontend/src/components/MakeSugestao.vue
+++ Frontend/src/components/MakeSugestao.vue
@@ -1,46 +1,88 @@
 <template>
-  <div class="wrapper">
-    <div class="title-area">
-      <h1 class="h1">Enviar Sugestão</h1>
-      <p class="p1">
-        Este formulário serve para comunicar diretamente com a USF.</p>
-    </div>
-    <div class="form-area">
-    <form id="formMed">
-      <div class="info-area">
-        <label class="label">Título</label>
-        <input type="text" class="input-text" required v-model="titulo">
-        <span style="color: #ff5252; font-size: 12px;" v-if="false"> Título é um campo obrigatório.</span>
-        <label class="label">Sugestão</label>
-        <textarea v-model="sugestao" class="input-box">
+  <div>
+    <!-- 
+      Modals 
+    -->
+    <modal-message
+      title="Remover?"
+      :visible="modalConfirm"
+      options
+      @close="modalConfirm = false"
+      @confirm="sendSugestao"
+    >
+      Deseja submeter esta sugestão?
+    </modal-message>
+    <modal-message
+      title="Sucesso"
+      :visible="modal"
+      @close="goHome"
+    >
+      Sugestão enviada com sucesso
+    </modal-message>
+    <modal-message
+      title="Erro"
+      :visible="modalError"
+      @close="this.modalError = false"
+    >
+      Erro ao enviar sugestão
+    </modal-message>
 
-        </textarea>
-        <span style="color: #ff5252; font-size: 12px;" v-if="false">Sugestão é um campo obrigatório.</span>
-        <div>
-          <v-btn class="button" @click="sendSugestao">Submeter</v-btn>
+    <!-- 
+      Conteudo
+    -->
+    <div class="wrapper">
+      <div class="title-area">
+        <h1 class="h1">Enviar Sugestão</h1>
+        <p class="p1">
+          Este formulário serve para comunicar diretamente com a USF.</p>
+      </div>
+      <div class="form-area">
+      <form id="formMed">
+        <div class="info-area">
+          <label class="label">Título</label>
+          <input type="text" class="input-text" required v-model="titulo" @blur="checkTitulo" @input="missTitulo=false" />
+          <span style="color: #ff5252; font-size: 12px;" v-if="missTitulo"> Título é um campo obrigatório.</span>
+
+          <label class="label">Sugestão</label>
+          <textarea required v-model="sugestao" class="input-box" @blur="checkSugestao" @input="missSugestao=false" />
+          <span style="color: #ff5252; font-size: 12px;" v-if="missSugestao">Sugestão é um campo obrigatório.</span>
+
+          <div>
+            <v-btn class="button" @click="modalConfirm=true">Submeter</v-btn>
+          </div> 
         </div> 
-      </div> 
-    </form>
-    </div>
-  </div>  
+      </form>
+      </div>
+    </div>  
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
 import jwt from 'jsonwebtoken'
+import ModalMessage from './ModalMessage.vue'
 
   export default {
     name: "MakeSugestao",
+    components: {
+      ModalMessage
+    },
     data() {
       return {
         token: localStorage.getItem('jwt'),
         titulo:'',
-        sugestao:''
-        
+        sugestao:'',
+        missTitulo: false,
+        missSugestao: false,
+
+        modal: false,
+        modalConfirm: false,
+        modalError: false
       }
     },
     methods: {
       sendSugestao (){
+        if(!this.checkRequired()) return false
         let user = jwt.decode(this.token)
         let body = {
           user: user._id,
@@ -50,11 +92,42 @@ import jwt from 'jsonwebtoken'
         }
         axios.post("http://localhost:3333/sugestao", body, {headers:{'authorization':'Bearer '+ this.token}} )
         .then(() => {
-          this.$route.go()
+          this.modalConfirm = false
+          this.modal = true
         })
         .catch(err => {
+          this.modalConfirm = false
+          this.modalError = true
           console.log(JSON.stringify(err))
         })
+      },
+      checkRequired () {
+        let titulo = this.checkTitulo()
+        let sugestao = this.checkSugestao()
+
+        if(!titulo)
+          return false
+        if(!sugestao)
+          return false
+        return true
+      },
+      checkTitulo () {
+        if(!this.titulo) {
+          this.missTitulo = true
+          return false
+        }
+        return true
+      },
+      checkSugestao () {
+        if(!this.sugestao) {
+          this.missSugestao = true
+          return false
+        }
+        return true
+      },
+      goHome () {
+        this.modal=false
+        this.$router.push('/')
       }
    }
   
